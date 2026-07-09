dafda71307b7ce01bdc0282969abbba854561dc9
snapdragon-MUJ <vishalvivekdudes@gmail.com>
2026-07-09 21:38:55 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a=""
+
+        for c in s:
+            if c.isalnum():
+                a += c.lower()
+
+        return a == a[::-1]        
